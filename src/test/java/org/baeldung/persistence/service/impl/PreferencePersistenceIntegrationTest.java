8d5e06cbd7fc9d49dae4b2976e2c2bf0330bd1c2
eugenp <hanriseldon@gmail.com>
2015-06-25 20:41:55 +0100

    integration testing work

--- src/test/java/org/baeldung/persistence/service/impl/PreferencePersistenceIntegrationTest.java
+++ src/test/java/org/baeldung/persistence/service/impl/PreferencePersistenceIntegrationTest.java
@@ -2,21 +2,20 @@ package org.baeldung.persistence.service.impl;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
-import org.baeldung.common.AbstractServicePersistenceIntegrationTest;
+import org.baeldung.common.AbstractPersistenceIntegrationTest;
 import org.baeldung.persistence.TestJpaConfig;
 import org.baeldung.persistence.dao.PreferenceRepository;
 import org.baeldung.persistence.model.Preference;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { TestJpaConfig.class }, loader = AnnotationConfigContextLoader.class)
-public class PreferencePersistenceIntegrationTest extends AbstractServicePersistenceIntegrationTest<Preference> {
+public class PreferencePersistenceIntegrationTest extends AbstractPersistenceIntegrationTest<Preference> {
 
 	@Autowired
 	private PreferenceRepository preferenceRepository;
@@ -26,16 +25,6 @@ public class PreferencePersistenceIntegrationTest extends AbstractServicePersist
 	@Test
 	public final void whenContextIsBootstrapped_thenNoExceptions() {
 		//
-	}
-
-	@Test
-	public final void whenEntityIsCreated_thenNoExceptions() {
-		getApi().save(createNewEntity());
-	}
-
-	@Test(expected = DataIntegrityViolationException.class)
-	public final void whenInvalidEntityIsCreated_thenDataException() {
-		getApi().save(createInvalidNewEntity());
 	}
 
 	// API - protected
