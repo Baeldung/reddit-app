103437d555000a735c165843ec6475643648a381
DOHA <dohae243@gmail.com>
2015-10-26 17:49:49 +0200

    cleanup

--- reddit-web/java/org/baeldung/web/live/AbstractLiveTest.java
+++
@@ -1,9 +0,0 @@
-package org.baeldung.web.live;
-
-import org.baeldung.reddit.util.Dto;
-
-public abstract class AbstractLiveTest<D extends Dto> extends AbstractBaseLiveTest {
-
-    protected abstract D newDto() throws Exception;
-
-}
